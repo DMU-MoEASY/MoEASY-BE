7059bcce9f642cba0ecf39bae77a2b899b1fc386
Junhyuk K <kjh27256901@gmail.com>
2026-09-01 23:28:45 +0900

    chore: initialize spring boot project

---
+++ src/main/java/com/robotdelivery/moeasybe/MoEasyBeApplication.java
@@ -0,0 +1,13 @@
+package com.robotdelivery.moeasybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoEasyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoEasyBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robotdelivery/moeasybe/MoEasyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robotdelivery.moeasybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoEasyBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
