4aa1456f99b873d5b4637e82d85ec2dc57e6ca8a
Junhyuk K <kjh27256901@gmail.com>
2026-09-09 15:08:22 +0900

    feat: add swagger openapi documentation

---
+++ src/main/java/com/moeasy/moeasybe/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.moeasy.moeasybe.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "MoEASY API",
+                version = "v1",
+                description = "MoEASY 백엔드 API 문서"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
